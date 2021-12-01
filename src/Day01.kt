fefd04c2cf041f5542358265ccf526e806c4ab04
lukas prediger <lukas.prediger@rewe-digital.com>
2021-12-01 20:36:19 +0100

    Solution Day 1 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,14 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.zipWithNext { a, b -> a < b }.count { it }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val test_input = readInput("Day01_test")
+    check(part1(test_input) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
