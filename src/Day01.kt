d9ad6bc17c8f1d08102810f8cb174f9ba7749369
lukas prediger <lukas.prediger@rewe-digital.com>
2021-12-01 20:39:38 +0100

    Solution Day 1 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,11 +4,15 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.windowed(3)
+            .map { window -> window.sumOf { it.toInt() } }
+            .zipWithNext { a, b -> a < b }
+            .count { it }
     }
 
     val test_input = readInput("Day01_test")
     check(part1(test_input) == 7)
+    check(part2(test_input) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
